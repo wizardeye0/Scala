ad5e5a2a517511c0104e07e2e67fd049feae113d
air seok <wizard_i@naver.com>
2017-08-04 20:31:45 +0900

    Queue Trait add

--- chap19/FunctionalQueue.scala
+++ chap19/FunctionalQueue.scala
@@ -1,24 +1,34 @@
-class Queue[T] (
-	private val leading: List[T],
-	private val trailing: List[T]
-) {
-	private def mirror = 
-		if (leading.isEmpty)
-			new Queue(trailing.reverse, Nil)
-		else
-			this
-	def head = mirror.leading.head
-	def tail = {
-		val q = mirror
-		new Queue(q.leading.tail, q.trailing)
-	}
-	def enqueue(x: T) = new Queue(leading, x::trailing)
+trait Queue[T] {
+	def head: T
+	def tail: Queue[T]
+	def enqueue(x: T): Queue[T]
 }
 
 object Queue {
-	def apply[T] (xs: T*) = new Queue[T] (xs.toList, Nil)
+	def apply[T] (xs: T*): Queue[T] = 
+		new QueueImpl[T] (xs.toList, Nil)
 
+	private class QueueImpl[T] (
+		private val leading: List[T],
+		private val trailing: List[T]
+	) extends Queue[T] {
+		def mirror = 
+			if (leading.isEmpty)
+				new QueueImpl(trailing.reverse, Nil)
+			else 
+				this
+		def head: T = mirror.leading.head
+		def tail: QueueImpl[T] = {
+			val q = mirror
+			new QueueImpl(q.leading.tail, q.trailing)
+		}
+		def enqueue(x: T) = 
+			new QueueImpl(leading, x::trailing)
+	}
+}
+
+object FnQueue {
 	def main(args: Array[String]) {
-		println(apply(1,2,3).head)
+		println(Queue.apply(1,2,3).head)
 	}
 }
